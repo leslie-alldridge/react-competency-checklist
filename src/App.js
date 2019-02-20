95c5bbb35015dbc33dccde30f3e83db70d19b8cd
Nat Grant <email.natgrant@gmail.com>
2019-02-20 21:36:51 +1300

    adding nav to app component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import React, { Component } from 'react';
-import './App.css';
-import Beginner from './Beginner'
+import React, { Component } from "react";
+import Beginner from "./Beginner";
+import MainNav from "./components/MainNav";
 
 class App extends Component {
   state = {
     beginner: false,
     intermediate: false,
     advanced: false
-  }
+  };
 
   toggleClick = () => {
     this.setState({
       beginner: !this.state.beginner
-    })
-  }
+    });
+  };
 
   render() {
     return (
       <div className="App">
         <header className="App-header">
-        <Beginner />
+          <MainNav />
+          <Beginner />
         </header>
       </div>
     );
